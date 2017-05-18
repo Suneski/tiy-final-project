e05d8efb9c727bd758930939018634cbc002844e
David Espinoza <despinoza1027@gmail.com>
2017-05-18 18:23:47 -0500

    updated server

--- react-ui/src/SavedRestaurants.js
+++ react-ui/src/SavedRestaurants.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import $ from 'jquery';
 
 class SavedRestaurants extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      savedRestaurants: []
+    }
+  }
+
+  summonTheData() {
+    $.ajax({
+      url: '/api/savedrestaurants'
+    })
+    .done((data) => {
+      this.setState({
+        savedRestaurants: data
+      })
+      console.log('grabbing data', this.state.searchResults);
+    });
+  }
+
+  clickit() {
+    console.log(this.state.savedRestaurants);
+  }
+
+
   render() {
     return (
-      <div>TEST TEXT FOR SAVED RESTAURANTS</div>
+      <div onClick={() => this.clickit()}>TEST TEXT FOR SAVED RESTAURANTS</div>
     )
   }
 }
