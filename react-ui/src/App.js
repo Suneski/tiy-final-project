408f66d95460f25bac152675fd6357717e306c7d
David Espinoza <despinoza1027@gmail.com>
2017-05-18 14:32:05 -0500

    added react-router functionality

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import RestaurantQuery from './RestaurantQuery.js'
+import SavedRestaurants from './SavedRestaurants.js'
+
+var Navigation = () => {
+  return <nav>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/savedrestaurants">Saved Restaurants</Link></li>
+    </ul>
+  </nav>
+}
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <RestaurantQuery />
-      </div>
+      <Router>
+        <div>
+          <Navigation />
+          <div className="container">
+            <Route path="/" exact component={RestaurantQuery} />
+            <Route path="/savedrestaurants" component={SavedRestaurants} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ react-ui/src/SavedRestaurants.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SavedRestaurants extends React.Component {
+  render() {
+    return (
+      <div>TEST TEXT FOR SAVED RESTAURANTS</div>
+    )
+  }
+}
+
+module.exports = SavedRestaurants;
