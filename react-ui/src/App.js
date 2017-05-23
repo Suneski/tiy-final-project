9eb397c2856640a0a78bdc7e99b9528fdd5b0ebc
David Espinoza <despinoza1027@gmail.com>
2017-05-23 14:35:21 -0500

    added 404 page

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
 import './App.css';
 
-import RestaurantQuery from './RestaurantQuery.js'
-import SavedRestaurants from './SavedRestaurants.js'
+import RestaurantQuery from './RestaurantQuery.js';
+import SavedRestaurants from './SavedRestaurants.js';
 
+import notfound from './images/404.gif';
+
+var NotFound = () => (
+  <div>
+    <img src={notfound} alt="not found" className="not-found"/>
+  </div>
+)
 
 var Navigation = () => {
   return <nav>
@@ -22,8 +29,11 @@ class App extends Component {
         <div>
           <Navigation />
           <div className="container">
-            <Route path="/" exact component={RestaurantQuery} />
-            <Route path="/savedrestaurants" component={SavedRestaurants} />
+            <Switch>
+              <Route path="/" exact component={RestaurantQuery} />
+              <Route path="/savedrestaurants" component={SavedRestaurants} />
+              <Route component={NotFound} />
+            </Switch>
           </div>
         </div>
       </Router>
