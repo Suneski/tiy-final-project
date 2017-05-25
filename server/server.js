1abc4099b6219ae09a98f3abac74c1ae770138ab
David Espinoza <despinoza1027@gmail.com>
2017-05-24 19:06:45 -0500

    updated server.js

--- server/server.js
+++ server/server.js
@@ -19,6 +19,8 @@ app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
+require('./auth.js')(app);
+
 app.use(require('./restaurant-apis.js'));
 
 app.get('*', function(request, response) {
