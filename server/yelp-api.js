12b79fea129457a48d5591814bb0ba0b445894f5
David Espinoza <despinoza1027@gmail.com>
2017-05-26 17:30:57 -0500

    separted yelp api portion to own file

---
+++ server/yelp-api.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const Restaurant = require('./models/SavedRestaurants.js');
+
+
+const app = express.Router();
+
+// Answer API requests.
+app.get('/api/yelp/', function(req,res) {
+
+  console.log('is the data being summoned?');
+
+  'use strict';
+
+  var output = [];
+  var term = `${req.query.restaurantSearch}`;
+  var location = `${req.query.locationSearch}`;
+  var limit = `${req.query.resultLimit}` || '20';
+  var sort_by = `${req.query.sortBy}` || 'best_match';
+
+  const yelp = require('yelp-fusion');
+
+  // Place holders for Yelp Fusion's OAuth 2.0 credentials. Grab them
+  // from https://www.yelp.com/developers/v3/manage_app
+  const clientId = 'S0HZe9JeSX10-qMRUtM_4Q';
+  const clientSecret = 'ergT0KVaFxSVznE3xZ6kKz9lJwp7SOWe0tlPBheuCL66avlespWmv8zdm68Y5LN4';
+
+  const searchRequest = {
+    term: term,
+    location: location,
+    limit: limit,
+    sort_by: sort_by,
+  };
+
+  yelp.accessToken(clientId, clientSecret).then(response => {
+    const client = yelp.client(response.jsonBody.access_token);
+
+    client.search(searchRequest).then(response => {
+      for (var i = 0; i < response.jsonBody.businesses.length; i++) {
+        const result = response.jsonBody.businesses[i];
+        const prettyJson = JSON.stringify(result, null, 4);
+        output.push(result);
+      }
+      res.send({
+        data: output
+      })
+      console.log(output);
+    });
+  });
+});
+
+module.exports = app
