712260753ab8625a35e44f23a2b38d406b27c617
David Espinoza <despinoza1027@gmail.com>
2017-05-28 23:37:17 -0500

    refactored logout

--- react-ui/src/Logout.js
+++ react-ui/src/Logout.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import $ from 'jquery';
-
+import Api from './Api.js';
 
 import './index.css';
 
 class Logout extends React.Component {
-
-  handleLogout() {
-    $.ajax({
-      url: '/api/logout',
-      method: 'POST'
-    })
-    .done(() => {
-      console.log('Logout complete.');
-    });
-  }
-
   render() {
     return (
       <div className="logout navigationItems">
-        <button onClick={() => this.handleLogout()}>LOG OUT</button>
+        <button onClick={() => Api.handleLogout()}>LOG OUT</button>
       </div>
     );
   }
